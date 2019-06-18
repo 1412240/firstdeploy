7cb1d80f98ec8981d8ada826265511ba9eb25422
Pete Bui <bdkhanh1996@gmail.com>
2019-06-18 16:14:27 +0700

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,10 +4,9 @@ lock "~> 3.11.0"
 set :application, "deploy_app"
 set :repo_url, "https://github.com/1412240/firstdeploy"
 set :deploy_to, "/home/ubuntu/www/deploy_app"
-
+set :rbenv_path, "/home/ubuntu/.rbenv"
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
 
 
