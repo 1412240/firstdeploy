4615e28e096d74b1086a93083e7bfd38f51588ed
Pete Bui <bdkhanh1996@gmail.com>
2019-06-18 15:50:39 +0700

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,6 +7,7 @@ set :deploy_to, "/home/ubuntu/www/deploy_app"
 
 
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
 set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
 
 
