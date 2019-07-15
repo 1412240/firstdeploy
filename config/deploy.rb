a73d14b4f75eae424e56724845fe905bb6484235
Pete Bui <bdkhanh1996@gmail.com>
2019-07-15 15:30:31 +0700

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,25 +8,29 @@ set :deploy_to, "/home/ubuntu/www/myapp"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
 set :default_env, { path: "~/.rbenv/shims:~/.rbenv/bin:$PATH" }
-
-
 set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
 # Default value for :pty is false
 set :keep_releases, 5
-
-
 # Default value for :linked_files is []
 set :linked_files, %w[config/database.yml config/secrets.yml]
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 namespace :deploy do
+  desc 'start nginx'
+  task :start_nginx do
+    on roles(:app) do
+      sudo 'service nginx start'
+    end
+  end
+
   desc 'restart application'
   task :restart do
     on roles(:app) do
+      Run "sudo service nginx start"
       invoke 'puma:stop'
       invoke 'puma:start'
     end
   end
-end
 
+  after :finishing, :start_nginx
+end
